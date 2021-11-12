2aa424d1dfe311c76fa235d36f8eed00d339aa0f
まみー <mamy1326@gmail.com>
2021-11-13 03:30:49 +0900

    add: adminlte config

---
+++ backend/config/adminlte.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'Theme' => [
+        'title' => '【管理画面】',
+        'logo' => [
+            'mini' => '<b>L</b>',
+            'large' => "<img src='/img/logo.png' alt='logo' style='width:110px'>",
+        ],
+        'login' => [
+            'show_remember' => true,
+            'show_register' => true,
+            'show_social' => true
+        ],
+        'folder' => ROOT,
+        'skin' => 'blue'
+    ]
+];
